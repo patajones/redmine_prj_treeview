ed06daf31af79b204383b5435108567a35b62f7e
patajones <ricardo.bernardes@gmail.com>
2018-08-16 14:58:30 -0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -3,6 +3,8 @@ require 'redmine_prj_treeview'
 
 Redmine::Plugin.register :redmine_prj_treeview do
   name 'Redmine Prj Treeview plugin'
+  author 'Bernardes'  
+  author_url 'https://github.com/patajones'
   description 'Redmine plugin to show up the projects as a tree'
   version '1.1.0'
   requires_redmine :version_or_higher => '3.1.0'
